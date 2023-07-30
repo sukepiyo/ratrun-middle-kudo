8af9694209f2a1b2e0403e7cfef74db4dcc92d17
sukepiyo <keisukekudo0828@gmail.com>
2023-07-30 23:35:03 +0900

    レスポンシブ表示切替の実装

--- components/organisms/Footer/AppFooter.vue
+++ components/organisms/Footer/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <footer class="footer">
     <div class="footer__top">
-      <div class="footer__left">
-        <img src="~/assets/img/logo-2.png" alt="ロゴ" />
-        <p class="footer__copy">Interior design<span>&</span>Construction</p>
+      <div class="logo">
+        <img class="logo__img" src="~/assets/img/logo-2.png" alt="ロゴ" />
+        <p class="logo__copy">Interior design<span>&</span>Construction</p>
       </div>
-      <ul class="footer__list">
+      <ul class="list">
         <span>
           <li v-for="(page, key) in pageList.slice(0, 5)" :key="key">
             <nuxt-link :to="page.to">{{ page.title }}</nuxt-link>
@@ -17,28 +17,24 @@
           </li>
         </span>
       </ul>
-      <div class="footer__right">
-        <div class="footer__block">
-          <p>
-            建築・建築デザイン・原状回復・内装解体・建物解体・アスベスト除去工事はSENTIAにお任せください！
-          </p>
+      <div class="address">
+        <div class="address__text">
+          <p>建築・建築デザイン・原状回復・内装解体・建物解体・アスベスト除去工事はSENTIAにお任せください！</p>
           <p>株式会社SENTIA</p>
           <p>〒454-0982</p>
           <p>愛知県名古屋市高見1-20-2</p>
           <p>TEL : 052-1212-2029</p>
         </div>
-        <ul class="footer__sns">
+        <ul class="address__sns">
           <!-- 要確認箇所-->
         </ul>
       </div>
     </div>
-    <div class="footer__bottom">
-      Copyright(c) 店舗設計・デザイン・施工CYPHER. AllRights Reserved
-    </div>
+    <div class="footer__bottom">Copyright(c) 店舗設計・デザイン・施工CYPHER. AllRights Reserved</div>
   </footer>
 </template>
 
- <script>
+<script>
 export default {
   setup() {
     const pageList = reactive([
@@ -55,38 +51,76 @@ export default {
   },
 };
 </script>
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .footer {
   margin-top: 200px; // レイアウト確認用
   background-color: #756d6d;
   &__top {
     margin: 0 auto;
-    padding: 20px;
-    width: min(100%, 1200px);
+    padding: 80px 40px; //後で調整
+    width: min(100%, 1400px);
     color: #fff;
     display: flex;
-    gap: 60px;
-  }
-  &__copy {
-    text-align: center;
-    span {
-      display: block;
+    gap: 40px;
+    & > * {
+      flex: 1;
     }
-  }
-  &__block {
-    border: double;
-  }
-  &__list {
-    display: flex;
-    gap: 60px;
-    & li {
-      margin-bottom: 1em;
+    @include mobile {
+      flex-direction: column;
     }
   }
   &__bottom {
     padding: 20px;
     background-color: #fff;
     text-align: center;
+    @include mobile {
+      text-align: left;
+    }
+  }
+}
+.logo {
+  text-align: center;
+  &__copy {
+    margin-top: 80px;
+    font-size: 1.6em;
+    span {
+      display: block;
+    }
+  }
+}
+.list {
+  display: flex;
+  gap: 60px;
+  & li {
+    margin-bottom: 1em;
+  }
+  @include mobile {
+    flex-direction: column;
+    gap: 0;
+  }
+}
+.address {
+  &__text {
+    outline: 2px solid #fff;
+    border: 2px solid #fff;
+    outline-offset: 6px;
+    padding: 1em;
+    & > *:first-of-type {
+      margin-bottom: 1em;
+    }
+    & > *:nth-child(2) {
+      font-size: 1.4em;
+    }
+    @include mobile {
+      outline: none;
+      padding: 2em 1em;
+      & > *:nth-child(2) {
+        margin: 1em 0 2em;
+      }
+      & > *:nth-child(4) {
+        margin-bottom: 1em;
+      }
+    }
   }
 }
 </style>
